2d8343e78279b08790e690d59fe9762177f3336d
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2020-12-17 13:05:31 +0700

    Add files via upload

---
+++ exercise5_2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string text1, text2;
+	int N, count = 0;
+
+	cout << "Enter the first text: ";
+	cin >> text1;
+	cout << "Enter the second text: ";
+	cin >> text2;
+	cout << "Enter N: ";
+	cin >> N;
+
+	while (count < N)
+	{
+		if (count % 2 == 0)
+		{
+			cout << text1 << " ";
+
+		}
+		else
+		{
+			cout << text2 << " ";
+		}
+		count++;
+
+	}
+
+	return 0;
+}
